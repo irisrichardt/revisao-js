14fe807a38abeb79c7a1ad36f284cb08ecda6448
Íris <iris.balk04@gmail.com>
2023-02-16 18:14:41 -0300

    main
    atualizando rotas

--- 01-node_js/http/app.js
+++ 01-node_js/http/app.js
@@ -1,17 +1,17 @@
 import * as http from  'http';
 
+// rotas
 const server = http.createServer((req, res) => {
-    res.writeHead('200', {'Content-Type': 'text/html'});
+    res.writeHead('200', {'Contente-Type': 'text/html'});
     var url = req.url;
-
-    if(url == '/sobre') {
-        res.write('<h1>NodeJs Sobre</h1>');
+    if(url === '/sobre') {
+        res.write('<h1>NodeJs Sobre</h1><p>Saiba mais sobre o NodeJs!</p>');
         res.end();
-    }else if(url === '/contato'){
-        res.write('<h1>NodeJs Contato</h1>');
+    } else if(url === '/contato') {
+        res.write('<h1>NodeJs Contato</h1><p>Entre em contato!</p>');
         res.end();
-    }else {
-        res.write('<h1>NodeJs</h1><ul><li><a href="/sobre">Sobre</a></li><li><a href="/contato">contato</a></li></ul>');
+    } else {
+        res.write('<h1>NodeJs</h1><p>Ola, Web Developer!</p><ul><li><a href="/sobre">Sobre</a></li><li><a href="/contato">Contato</a></li></ul>');
         res.end();
     }
 });
